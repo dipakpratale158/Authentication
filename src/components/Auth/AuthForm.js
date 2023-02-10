842a8981e50260ce11db6e3911082418b96a278f
sravanisravs1 <kodurusravani813@gmail.com>
2023-02-10 19:26:22 +0530

    added auto logout

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,7 +1,7 @@
 import { useState, useRef, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import AuthContext from '../../store/Auth-context'
+import AuthContext from '../../store/Auth-context';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
@@ -12,97 +12,95 @@ const AuthForm = () => {
   const authCtx = useContext(AuthContext);
 
   const [isLogin, setIsLogin] = useState(true);
-  const [loading , setLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const submitHandler = (e) => {
-    e.preventDefault()
+  const submitHandler = (event) => {
+    event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
- setLoading(true);
- let url;
-    if (isLogin) {
-      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRorahRJ_juKatl8aQXmkip1wxxy28S84'
-    } else {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDRorahRJ_juKatl8aQXmkip1wxxy28S84'
-    }
+    // optional: Add validation
 
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRorahRJ_juKatl8aQXmkip1wxxy28S84';
+    } else {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDRorahRJ_juKatl8aQXmkip1wxxy28S84';
+    }
     fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: enteredEmail,
         password: enteredPassword,
-        returnSecureToken : true
+        returnSecureToken: true,
       }),
       headers: {
-        'Content-Type' : 'application/json'
-      }
-    }).then(res => {
-         setLoading(false);
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication failed!';
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
 
-      if (res.ok) {
-        console.log(res)
-        alert('Succesful')
-        return res.json();
-      } else {
-        res.json().then(data => {   // Json also return promises milan
-          let errorMessage = 'Authentication failed'
-          if(data && data.error && data.error.message) {
-          errorMessage = data.error.message
-          alert(errorMessage)
-          }
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        const expirationTime = new Date(
+          new Date().getTime() + +data.expiresIn * 1000
+        );
+        authCtx.login(data.idToken, expirationTime.toISOString());
+        history.replace('/');
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
-        throw new Error(errorMessage);
-        })
-      }
-    })
-    .then((data) => {
-      //console.log(data.idToken)
-      authCtx.login(data.idToken);
-      history.replace('/');
-    })
-    .catch((err) => {
-      alert(err.message)
-    })
-  }
-// console.log(loading)
-  
-return (
+  return (
     <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input
-            ref={emailInputRef}
-            type="email"
-            id="email"
-            required
-          />
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
+          <label htmlFor='password'>Your Password</label>
           <input
-            ref={passwordInputRef}
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             required
+            ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
-          {loading && <p>Sending request...</p> }
-          {!loading && <button>{isLogin ? "Login" : "Create Account"}</button>}
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
-            type="button"
+            type='button'
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? "Create new account" : "Login with existing account"}
+            {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
       </form>
--- src/store/Auth-context.js
+++ src/store/Auth-context.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
+let logoutTimer;
 
 const AuthContext = React.createContext({
   token: '',
@@ -8,23 +9,71 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
+const retrieveStoredToken = () => {
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationDate = localStorage.getItem('expirationTime');
+
+  const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+  if (remainingTime <= 3600) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    duration: remainingTime,
+  };
+};
+
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem('token')
+  const tokenData = retrieveStoredToken();
+  
+  let initialToken;
+  if (tokenData) {
+    initialToken = tokenData.token;
+  }
+
   const [token, setToken] = useState(initialToken);
 
-  const userIsLoggedIn = !!token;//if token is null,it will store false
+  const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token",token); //on login ,we get token and stored using state
-  };
-
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
-    console.log('Logged Out')
     localStorage.removeItem('token');
-     //on logout token should be set to null
+    localStorage.removeItem('expirationTime');
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+    localStorage.setItem('expirationTime', expirationTime);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+
+  useEffect(() => {
+    if (tokenData) {
+      console.log(tokenData.duration);
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  }, [tokenData, logoutHandler]);
 
   const contextValue = {
     token: token,
